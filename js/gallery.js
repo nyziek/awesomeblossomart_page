bbf5197ee98a91cef1e2e675cbbbbbd675c57e03
Tomasz Nyżnyk <tomasznyznyk@gmail.com>
2019-09-05 00:51:20 +0200

    Added image galleries. Made header stick to top of the page. Modified layout to fit page with sticky header.

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+//VARIABLES
+
+const gallery = document.querySelector(".galleria");
+
+//METHODS
+
+for(let i=1; i<14; i++) {
+
+    const newImg = document.createElement('img');
+
+    newImg.setAttribute('src', './images/gallery/adults/' + i + '.jpg');
+
+    gallery.appendChild(newImg)
+}
--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-//VARIABLES
+//MENU
+
+//Variables
 
 const hamburgerMenuIcon = document.querySelector(".hamburgerMenuIcon");
 const menu = document.querySelector(".menu");
 
-//METHODS
+//Methods
 
 function hamburgerMenuClickHandle() {
     hamburgerMenuIcon.classList.toggle("change");
@@ -15,7 +17,47 @@ function menuHide() {
     hamburgerMenuIcon.classList.toggle("change");
 }
 
-//PAGE LOGIC
+//Logic
 
 hamburgerMenuIcon.addEventListener("click", hamburgerMenuClickHandle);
 menu.addEventListener("click", menuHide);
+
+//GALLERIES SLIDER
+
+//Variables
+
+const slides = document.querySelectorAll(".galleriesSlide");
+const sliderNavLeft = document.querySelector(".galleriesNavLeft");
+const sliderNavRight = document.querySelector(".galleriesNavRight");
+let slideIndex = 0;
+
+//Methods
+
+const slideChangeNext = () => {
+
+    slides[slideIndex].classList.remove("activeSlide");
+    slideIndex++;
+
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    slides[slideIndex].classList.add("activeSlide");
+};
+
+const slideChangePrev = () => {
+
+    slides[slideIndex].classList.remove("activeSlide");
+    slideIndex--;
+
+    if (slideIndex < 0) {
+        slideIndex = slides.length -1;
+    }
+
+    slides[slideIndex].classList.add("activeSlide");
+};
+
+//Logic
+
+sliderNavLeft.onclick = () => {slideChangePrev()};
+sliderNavRight.onclick = () => {slideChangeNext()};
